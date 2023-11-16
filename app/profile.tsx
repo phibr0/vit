379aad25dc39c6cfdf24c08e89b5c05cb4e6744a
Yakup Yazir <yakupyazir@MacBook-Pro-von-Yakup.local>
2023-11-16 23:53:27 +0100

    asd

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,14 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform } from 'react-native';
+import { useState } from "react";
+import { Platform } from "react-native";
 
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from "react-native";
+import { useLocalStorage } from "./(tabs)";
 
 export default function ModalScreen() {
-  return (
-    <View className="">
-      <Text className="">Profile</Text>
+  const { data } = useLocalStorage("account", null);
+  const [status, setStatus] = useState("profile");
 
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+  if (!data) return <Text>Loading</Text>;
+
+  return (
+    <View className="h-screen w-screen">
+      <View className="flex flex-row justify-between mt-4">
+        <TouchableOpacity onPress={() => setStatus("profile")}>
+          <Text className="text-xl ml-4">Profile</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setStatus("friends")}>
+          <Text className="text-xl mr-4">Friends</Text>
+        </TouchableOpacity>
+      </View>
+
+      {status === "profile" ? (
+        <View className="flex flex-col">
+          <View className="flex flex-row">
+            <Text>Name: </Text>
+            <Text>{data.name}</Text>
+          </View>
+
+          <View className="flex flex-row">
+            <Text>Geschlecht: </Text>
+            <Text>{data.gender}</Text>
+          </View>
+
+          <View className="flex flex-row">
+            <Text>Gewicht :</Text>
+            <Text>{data.weight}</Text>
+          </View>
+        </View>
+      ) : (
+        <View>
+          <Text>Friends</Text>
+        </View>
+      )}
+
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
 }
