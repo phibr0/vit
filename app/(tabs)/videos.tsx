b751c4ba7f807ab60a00028812cfe90c7d7ea516
Yakup Yazir <yakupyazir@MacBook-Pro-von-Yakup.local>
2023-11-16 21:35:36 +0100

    new4

--- app/(tabs)/videos.tsx
+++ app/(tabs)/videos.tsx
@@ -1,12 +1,89 @@
 import { SafeAreaView, StyleSheet } from "react-native";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, ScrollView, View, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function TabOneScreen() {
   return (
-    <SafeAreaView>
-      <View className="">
-        <Text className="">videos</Text>
-      </View>
+    <SafeAreaView className="h-full">
+      <ScrollView className="h-full">
+        <Text className="text-3xl">videos</Text>
+        <View className="flex flex-col w-screen justify-center items-center">
+          <View className="h-96 w-80 bg-cyan-600 flex justify-center items-center rounded-2xl mt-10">
+            <Text className="text-xl text-center text-white justify-center">
+              Seil schwingen
+            </Text>
+            <View className=" h-60 flex w-3/4 items-center mt-4 mb-2">
+              <Image
+                className="w-48 h-full rounded shadow-xl"
+                source={require("./pic1.jpg")}
+              />
+            </View>
+            <TouchableOpacity className="">
+              <Text className="text-center text-xl text-white">Done!</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="h-96 w-80 bg-teal-600 flex justify-center items-center rounded-2xl mt-10">
+            <Text className="text-xl text-center text-white justify-center">
+              Seil schwingen
+            </Text>
+            <View className=" h-60 flex w-3/4 items-center mt-4 mb-2">
+              <Image
+                className="w-48 h-full rounded shadow-xl"
+                source={require("./pic2.jpg")}
+              />
+            </View>
+            <TouchableOpacity className="">
+              <Text className="text-center text-xl text-white">Done!</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="h-96 w-80 bg-cyan-600 flex justify-center items-center rounded-2xl mt-10">
+            <Text className="text-xl text-center text-white justify-center">
+              Seil schwingen
+            </Text>
+            <View className=" h-60 flex w-3/4 items-center mt-4 mb-2">
+              <Image
+                className="w-48 h-full rounded shadow-xl"
+                source={require("./pic3.jpg")}
+              />
+            </View>
+            <TouchableOpacity className="">
+              <Text className="text-center text-xl text-white">Done!</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="h-96 w-80 bg-fuchsia-600 flex justify-center items-center rounded-2xl mt-10">
+            <Text className="text-xl text-center text-white justify-center">
+              Seil schwingen
+            </Text>
+            <View className=" h-60 flex w-3/4 items-center mt-4 mb-2">
+              <Image
+                className="w-48 h-full rounded shadow-xl"
+                source={require("./pic4.jpg")}
+              />
+            </View>
+            <TouchableOpacity className="">
+              <Text className="text-center text-xl text-white">Done!</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="h-96 w-80 bg-cyan-600 flex justify-center items-center rounded-2xl mt-10">
+            <Text className="text-xl text-center text-white justify-center">
+              Seil schwingen
+            </Text>
+            <View className=" h-60 flex w-3/4 items-center mt-4 mb-2">
+              <Image
+                className="w-48 h-full rounded shadow-xl"
+                source={require("./pic5.jpg")}
+              />
+            </View>
+            <TouchableOpacity className="">
+              <Text className="text-center text-xl text-white">Done!</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
