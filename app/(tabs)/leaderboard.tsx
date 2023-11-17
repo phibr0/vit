c3319716f5a99e274aa39cde8a5e6fc88774c1aa
Yakup Yazir <yakupyazir@MacBook-Pro-von-Yakup.local>
2023-11-17 06:15:14 +0100

    new8

--- app/(tabs)/leaderboard.tsx
+++ app/(tabs)/leaderboard.tsx
@@ -1,53 +1,33 @@
 import { ScrollView } from "tamagui";
-import React from "react";
 import { View, Text, FlatList, StyleSheet, SafeAreaView } from "react-native";
 import { DataTable } from "react-native-paper";
+import React, { useState, useEffect } from "react";
 
 const data = [
   {
     name: "John",
-    rank: 1,
-    currentHealth: 80,
-    level: 5,
     score: 1200,
   },
   {
     name: "Alice",
-    rank: 2,
-    currentHealth: 95,
-    level: 7,
     score: 1800,
   },
   {
-    name: "Alice",
-    rank: 3,
-    currentHealth: 95,
-    level: 7,
-    score: 1800,
+    name: "Alec",
+    score: 1400,
   },
   {
-    name: "Alice",
-    rank: 4,
-    currentHealth: 95,
-    level: 7,
-    score: 1800,
+    name: "Alina",
+    score: 1850,
   },
   {
-    name: "Alice",
-    rank: 5,
-    currentHealth: 95,
-    level: 7,
-    score: 1800,
+    name: "Ami",
+    score: 2000,
   },
   {
-    name: "Alice",
-    rank: 6,
-    currentHealth: 95,
-    level: 7,
-    score: 1800,
+    name: "Kamil",
+    score: 1400,
   },
-
-  // ... Weitere Datenzeilen nach Bedarf hinzufügen (Api am besten mit den Daten der Freunde )
 ];
 
 export default function TabOneScreen() {
@@ -55,24 +35,29 @@ export default function TabOneScreen() {
     <SafeAreaView className="h-full">
       <ScrollView className="h-full">
         <View className="pb-80">
-          <DataTable className="h-full">
-            <DataTable.Header>
-              <DataTable.Title>Rang</DataTable.Title>
-              <DataTable.Title>Name</DataTable.Title>
-              <DataTable.Title>Health</DataTable.Title>
-              <DataTable.Title>Level</DataTable.Title>
-              <DataTable.Title>Score</DataTable.Title>
-            </DataTable.Header>
+          <DataTable className="w-screen">
+            {data
+              .sort((a, b) => b.score - a.score)
+              .map((user, index) => (
+                <DataTable.Row key={index}>
+                  <View className="w-screen h-32 flex flex-row justify-between items-center rounded-2xl">
+                    <View className="bg-white h-20 w-16 ml-4 shadow-xl rounded-2xl ">
+                      <DataTable.Cell className="flex justify-center items-center rounded-2xl">
+                        #{index + 1}
+                      </DataTable.Cell>
+                    </View>
 
-            {data.map((user: any) => (
-              <DataTable.Row>
-                <DataTable.Cell>{user.rank}</DataTable.Cell>
-                <DataTable.Cell>{user.name}</DataTable.Cell>
-                <DataTable.Cell>{user.currentHealth}</DataTable.Cell>
-                <DataTable.Cell>{user.level}</DataTable.Cell>
-                <DataTable.Cell>{user.score}</DataTable.Cell>
-              </DataTable.Row>
-            ))}
+                    <View className="bg-white flex flex-row w-72 h-20 mr-14 shadow-xl  rounded-2xl">
+                      <DataTable.Cell className="flex justify-center items-center rounded-2xl">
+                        {user.name}
+                      </DataTable.Cell>
+                      <DataTable.Cell className="flex justify-center items-center   rounded-2xl">
+                        {user.score}
+                      </DataTable.Cell>
+                    </View>
+                  </View>
+                </DataTable.Row>
+              ))}
           </DataTable>
         </View>
       </ScrollView>
