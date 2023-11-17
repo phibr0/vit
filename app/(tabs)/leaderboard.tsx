9ae9389ec7c4250a11e3fb724292ab2aa28aa3a0
Hack4Pott Team 8 <nickson.onyango@thyssenkrupp.com>
2023-11-17 06:22:47 +0100

    wi

--- app/(tabs)/leaderboard.tsx
+++ app/(tabs)/leaderboard.tsx
@@ -1,7 +1,7 @@
-import { ScrollView } from 'tamagui';
 import React from 'react';
-import { View, Text, FlatList, StyleSheet, SafeAreaView } from 'react-native';
+import { ImageBackground, SafeAreaView, View } from 'react-native';
 import { DataTable } from 'react-native-paper';
+import { H1, ScrollView } from 'tamagui';
 
 const data = [
   {
@@ -50,35 +50,40 @@ const data = [
 
 export default function TabOneScreen() {
   return (
-    <SafeAreaView className="h-full">
-      <ScrollView className="h-full">
-        <View className="pb-80">
-          <DataTable className="w-screen">
-            {data
-              .sort((a, b) => b.score - a.score)
-              .map((user, index) => (
-                <DataTable.Row key={index}>
-                  <View className="w-screen h-32 flex flex-row justify-between items-center rounded-2xl">
-                    <View className="bg-white h-20 w-16 ml-4 shadow-xl rounded-2xl ">
-                      <DataTable.Cell className="flex justify-center items-center rounded-2xl">
-                        #{index + 1}
-                      </DataTable.Cell>
-                    </View>
+    <ImageBackground source={require('./leaderboard.png')}>
+      <SafeAreaView className="h-full">
+        <ScrollView className="h-full">
+          <H1 mt="$4" textAlign="center">
+            Leaderboard
+          </H1>
+          <View className="pb-80">
+            <DataTable className="w-screen">
+              {data
+                .sort((a, b) => b.score - a.score)
+                .map((user, index) => (
+                  <DataTable.Row key={index}>
+                    <View className="w-screen h-32 flex flex-row justify-between items-center rounded-2xl">
+                      <View className="bg-white h-20 w-16 ml-4 shadow backdrop-blur-md rounded-2xl ">
+                        <DataTable.Cell className="flex justify-center items-center rounded-2xl">
+                          #{index + 1}
+                        </DataTable.Cell>
+                      </View>
 
-                    <View className="bg-white flex flex-row w-72 h-20 mr-14 shadow-xl  rounded-2xl">
-                      <DataTable.Cell className="flex justify-center items-center rounded-2xl">
-                        {user.name}
-                      </DataTable.Cell>
-                      <DataTable.Cell className="flex justify-center items-center   rounded-2xl">
-                        {user.score}
-                      </DataTable.Cell>
+                      <View className="bg-white flex flex-row w-72 h-20 mr-14 shadow-xl  rounded-2xl">
+                        <DataTable.Cell className="flex justify-center items-center rounded-2xl">
+                          {user.name}
+                        </DataTable.Cell>
+                        <DataTable.Cell className="flex justify-center items-center   rounded-2xl">
+                          {user.score}
+                        </DataTable.Cell>
+                      </View>
                     </View>
-                  </View>
-                </DataTable.Row>
-              ))}
-          </DataTable>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+                  </DataTable.Row>
+                ))}
+            </DataTable>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
